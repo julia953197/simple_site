46a58c1166fcd642137d310a5b415fb90b846f5b
julia953197 <julia953197@yandex.ru>
2021-03-09 06:46:02 +0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+    //Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -30,4 +31,25 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    //Timer
+    const deadline = '2020-03-10';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date());
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock()
 });
